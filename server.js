416c8c818d38a392074d02e7a9538a201432160f
Mizuki <mizukikeda@gmail.com>
2023-02-07 18:47:10 +0100

    back: express + /projects

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 // libraries
+const express = require('express');
 
 // projects data
+const projects = require('./projects.json');
 
 // vars
+const app = express();
+const port = 3001;
 
+/* Routes */
+// Liste des projets : GET /
+app.get('/projects', (req,res) => {
+    console.log('>>GET /projects');
+    res.json(projects);
+});
+
+/*
+ * Server
+ */
+app.listen(port, () => {
+    console.log(`listening on *:${port}`)
+})
