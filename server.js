a2b6deba42fd99d3d3192e9d6d4e0c8c4122b778
Mizuki <mizukikeda@gmail.com>
2023-02-08 15:50:10 +0100

    axios link to back server

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const projects = require('./projects.json');
 const app = express();
 const port = 3001;
 
+// cors
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
+
 /* Routes */
 // Liste des projets : GET /
 app.get('/projects', (req,res) => {
